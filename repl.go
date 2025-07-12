f76eaba98a37afdcb5e2ed9cb1cd03f90c63a550
frozendolphin <122843077+frozendolphin@users.noreply.github.com>
2025-07-12 06:23:50 -0700

    implemented a simpleREPL

--- repl.go
+++ repl.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"strings"
+	"os"
+	"bufio"
+	"fmt"
 )
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func simpleREPL() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			userinput := scanner.Text()
+			cleaned_slice := cleanInput(userinput)
+			if len(cleaned_slice) == 0 {
+				continue
+			}
+			first_word := cleaned_slice[0]
+			fmt.Println("Your command was:", first_word)
+		}
+	}
 }
